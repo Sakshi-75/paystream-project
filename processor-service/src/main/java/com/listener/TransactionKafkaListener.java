7c6cea1eb3cb5ecc67714177022aa6f0cf2d1155
Sakshi Jain <sakshi.jain7597@gmail.com>
2025-11-21 13:46:19 +0530

    setup dlq for failed transactions and errors

--- processor-service/src/main/java/com/listener/TransactionKafkaListener.java
+++ processor-service/src/main/java/com/listener/TransactionKafkaListener.java
@@ -3,6 +3,7 @@ package com.listener;
 import com.service.TransactionProcessorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,6 +12,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class TransactionKafkaListener {
     @Autowired
     TransactionProcessorService transactionProcessorService;
+    @Autowired
+    KafkaTemplate<String, String> dlqKafkaTemplate;
 
     @Transactional("kafkaTransactionManager")
     @KafkaListener(topics = "transactions-input", groupId = "paystream-processor-group-v7")
@@ -21,6 +24,7 @@ public class TransactionKafkaListener {
             transactionProcessorService.validateTransaction(transaction);
         } catch (Exception e) {
             System.out.println("Processing failed for transaction "+ transaction + ". Routing to DLQ: " + e);
+            dlqKafkaTemplate.send("transactions-dlq", (transaction+" | Error: "+ e.getMessage()));
             throw new RuntimeException("Unrecoverable error, triggering transaction rollback.", e);
         }
     }
